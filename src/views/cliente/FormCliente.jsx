a4756a501bd8959217fa820f35886450dffc119c
raul-henrique <raul.silva@gdstecnologia.com.br>
2026-02-25 21:02:23 -0300

    Add axios for API requests and update FormCliente component

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -1,98 +1,126 @@
+import axios from 'axios';
 import InputMask from 'comigo-tech-react-input-mask';
-import React from "react";
+import React, { useState } from 'react';
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 
 export default function FormCliente() {
+  const [nome, setNome] = useState();
+  const [cpf, setCpf] = useState();
+  const [dataNascimento, setDataNascimento] = useState();
+  const [foneCelular, setFoneCelular] = useState();
+  const [foneFixo, setFoneFixo] = useState();
 
-    return (
-        <div>
-            <div style={{ marginTop: '3%' }}>
-                <Container textAlign='justified' >
-                    <h2> <span style={{ color: 'darkgray' }}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+  function salvar() {
+    let clienteRequest = {
+      nome: nome,
+      cpf: cpf,
+      dataNascimento: dataNascimento,
+      foneCelular: foneCelular,
+      foneFixo: foneFixo,
+    };
 
-                    <Divider />
+    axios
+      .post('http://localhost:8080/api/cliente', clienteRequest)
+      .then((response) => {
+        console.log('Cliente cadastrado com sucesso.');
+      })
+      .catch((error) => {
+        console.log('Erro ao incluir o um cliente.');
+      });
+  }
 
-                    <div style={{ marginTop: '4%' }}>
-                        <Form>
-                            <Form.Group widths='equal'>
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='Nome'
-                                    maxLength="100"
-                                />
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='CPF'>
-                                    <InputMask
-                                        required
-                                        mask="999.999.999-99"
-                                    />
-                                </Form.Input>
+  return (
+    <div>
+      <div style={{ marginTop: '3%' }}>
+        <Container textAlign="justified">
+          <h2>
+            {' '}
+            <span style={{ color: 'darkgray' }}>
+              {' '}
+              Cliente &nbsp;
+              <Icon name="angle double right" size="small" />{' '}
+            </span>{' '}
+            Cadastro{' '}
+          </h2>
 
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Input
-                                    fluid
-                                    label='Fone Celular'
-                                    width={6}>
-                                    <InputMask
-                                        mask="(99) 9999.9999"
-                                    />
-                                </Form.Input>
-                                <Form.Input
-                                    fluid
-                                    label='Fone Fixo'
-                                    width={6}>
-                                    <InputMask
-                                        mask="(99) 9999.9999"
-                                    />
-                                </Form.Input>
-                                <Form.Input
-                                    fluid
-                                    label='Data Nascimento'
-                                    width={6}
-                                >
-                                    <InputMask
-                                        mask="99/99/9999"
-                                        maskChar={null}
-                                        placeholder="Ex: 20/03/1985"
-                                    />
-                                </Form.Input>
-                            </Form.Group>
-                        </Form>
+          <Divider />
 
-                        <div style={{ marginTop: '4%' }}>
-                            <Button
-                                type="button"
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='orange'
-                            >
-                                <Icon name='reply' />
-                                Voltar
-                            </Button>
+          <div style={{ marginTop: '4%' }}>
+            <Form>
+              <Form.Group widths="equal">
+                <Form.Input
+                  required
+                  fluid
+                  label="Nome"
+                  maxLength="100"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                />
+                <Form.Input required fluid label="CPF">
+                  <InputMask
+                    required
+                    mask="999.999.999-99"
+                    value={cpf}
+                    onChange={(e) => setCpf(e.target.value)}
+                  />
+                </Form.Input>
+              </Form.Group>
+              <Form.Group>
+                <Form.Input fluid label="Fone Celular" width={6}>
+                  <InputMask
+                    mask="(99) 9999.9999"
+                    value={foneCelular}
+                    onChange={(e) => setFoneCelular(e.target.value)}
+                  />
+                </Form.Input>
+                <Form.Input fluid label="Fone Fixo" width={6}>
+                  <InputMask
+                    mask="(99) 9999.9999"
+                    value={foneFixo}
+                    onChange={(e) => setFoneFixo(e.target.value)}
+                  />
+                </Form.Input>
+                <Form.Input fluid label="Data Nascimento" width={6}>
+                  <InputMask
+                    mask="99/99/9999"
+                    maskChar={null}
+                    placeholder="Ex: 20/03/1985"
+                    value={dataNascimento}
+                    onChange={(e) => setDataNascimento(e.target.value)}
+                  />
+                </Form.Input>
+              </Form.Group>
+            </Form>
 
-                            <Button
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='blue'
-                                floated='right'
-                            >
-                                <Icon name='save' />
-                                Salvar
-                            </Button>
-                        </div>
-                    </div>
-                </Container>
+            <div style={{ marginTop: '4%' }}>
+              <Button
+                type="button"
+                inverted
+                circular
+                icon
+                labelPosition="left"
+                color="orange"
+              >
+                <Icon name="reply" />
+                Voltar
+              </Button>
+
+              <Button
+                inverted
+                circular
+                icon
+                labelPosition="left"
+                color="blue"
+                floated="right"
+                onClick={() => salvar()}
+              >
+                <Icon name="save" />
+                Salvar
+              </Button>
             </div>
-        </div>
-
-    );
-
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 }
